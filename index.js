1b491a2668ff7ba47c1d491ec1a7ecbdbe5be989
Eklavya-sus <95964894+Eklavya-sus@users.noreply.github.com>
2024-07-01 19:51:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import routes from "./routes/routes.js";
-import cron from "node-cron";
-import User from "./models/UserModel.js";
 import "dotenv/config.js";
 
 const app = express();
@@ -11,30 +9,8 @@ const PORT = 4000;
 const MONGODB_URI = process.env.MONGODB_CONNECTION_STRING;
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use("/", routes);
-
-// Schedule the cleanup task to run every minute
-cron.schedule("* * * * *", async () => {
-  const fiveMinutesAgo = new Date();
-  fiveMinutesAgo.setMinutes(fiveMinutesAgo.getMinutes() - 5);
-
-  // Find unverified user records created more than 5 minutes ago
-  const unverifiedUsers = await User.find({
-    isVerified: false,
-    createdAt: { $lt: fiveMinutesAgo },
-  });
-
-  console.log("Users going to be deleted : ", unverifiedUsers);
-  // Delete unverified user records
-  await User.deleteMany({
-    _id: { $in: unverifiedUsers.map((user) => user._id) },
-  });
-
-  console.log("Cleanup task completed.");
-});
 
 mongoose
   .connect(MONGODB_URI)
@@ -45,5 +21,5 @@ mongoose
     });
   })
   .catch((error) => {
-    console.log(error.message);
+    console.error("MongoDB connection error:", error);
   });
