127678ba72cdb62c29ce29af056096e0ad452300
Ishita <ishitaraina18@gmail.com>
2024-07-23 19:48:19 +0530

    revert

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import { signup, login } from "../controllers/AuthController.js";
 import { uploadArtwork } from "../controllers/ArtworkController.js";
 import { dashboardListArtworks } from "../controllers/DashboardController.js";
-import { listArtworksByTag } from "../controllers/ArtworkController.js"; 
 import { ArtworkDetailsController } from "../controllers/ArtworkDetailsController.js";
 import { ProfileDetails, ArtworksListedByUser } from "../controllers/ProfileController.js";
 import { DeleteArtwork } from "../controllers/DeleteArtworkController.js";
@@ -13,10 +12,10 @@ router.post("/api/auth/signup", signup);
 router.post("/api/auth/login", login);
 router.post("/api/upload", uploadArtwork);
 router.get("/api/dashboard", dashboardListArtworks);
-router.get("/api/items", listArtworksByTag); 
 router.post("/api/artworkDetails", ArtworkDetailsController);
 router.post("/api/profile", ProfileDetails);
 router.post("/api/artworksByUser", ArtworksListedByUser);
 router.post("/api/deleteArtwork", DeleteArtwork);
 
 export default router;
+
